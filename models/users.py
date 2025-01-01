bdbf667d829075a7c6b3a3a05cb3e71709164567
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-01 17:02:23 +0200

    Fix a typo

--- models/users.py
+++ models/users.py
@@ -15,7 +15,7 @@ class User(BaseModel, Base):
     dob = Column(Date, nullable=False)
     email = Column(String(50), nullable=False)
     password = Column(String(100), nullable=False)
-    image_path = Column(String(256), nullable=True, defualt=None)
+    image_path = Column(String(256), nullable=True, default=None)
     bio = Column(String(300), nullable=True)
 
     quizzes = relationship(
