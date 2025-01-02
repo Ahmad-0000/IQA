2f860ae54b97797dc331928a41e83424e232c8ce
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-02 05:02:59 +0200

    Ad 'Category' model

---
+++ models/categories.py
@@ -0,0 +1,12 @@
+"""Category model class
+"""
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+
+
+class Category(BaseModel, Base):
+    """Represent "categories" table
+    """
+    __tablename__ = 'categories'
+    name = Column(String(20), nullable=False)
