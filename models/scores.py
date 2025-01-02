30e591d16ac902e4014098e35944161e7442e2a5
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-02 04:58:21 +0200

    Add 'Score' model

---
+++ models/scores.py
@@ -0,0 +1,17 @@
+"""Score model class
+"""
+from sqlalchemy import Column, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+
+
+class Score(BaseModel, Base):
+    """Represent a score record
+    """
+    __tablename__ = 'scores'
+    score = Column(Float, nullable=False)
+    user_id = Column(String(36), ForeignKey('users.id'), nullable=False)
+    quiz_id = Column(String(36), ForeignKey('quizzes.id'), nullable=False)
+
+    user = relationship('User', back_populates='scores')
+    quiz = relationship('Quiz', back_populates='scores')
