076165c8c2614a91fd1a2607996e65d2d7ec6d50
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-04 14:34:19 +0000

    Add user endpoints tests
    Added tests for "GET /users", "GET /users/<user_id>", "POST /users"
    endpoints

--- tests/test_api/test_app.py
+++ tests/test_api/test_app.py
@@ -2,6 +2,9 @@
 """
 import unittest
 from api.v1.app import app
+from models import storage
+from models.users import User
+from uuid import uuid4
 
 
 class TestAPI(unittest.TestCase):
@@ -25,4 +28,41 @@ class TestAPI(unittest.TestCase):
         """
         error_404 = self.__class__.client.get("/none");
         self.assertEqual(error_404.status_code, 404)
+    
+    def test_new_user(self):
+        """Test "POST /api/v1/users"
+        """
+        res = self.__class__.client.post("/api/v1/users", json={
+                "first_name": "ahmad",
+                "middle_name": "husain",
+                "last_name": "basheer",
+                "dob": "2005-03-05",
+                "email": f"{str(uuid4())}@fake.fake",
+                "password": "fakepass"
+            });
+        self.assertEqual(res.status_code, 201)
+        user_email = res.json['email']
+        res = self.__class__.client.post("/api/v1/users", json={
+                "first_name": "ahmad",
+                "middle_name": "husain",
+                "last_name": "basheer",
+                "dob": "2005-03-05",
+                "email": user_email,
+                "password": "fakepass"
+            });
+        self.assertEqual(res.status_code, 409)
+    
+    def test_users(self):
+        """Test "GET /api/v1/users"
+        """
+        res = self.__class__.client.get("/api/v1/users")
+        self.assertEqual(res.status_code, 200)
+        self.assertIs(type(res.json), list)
 
+    def test_user(self):
+        """Test "GET /api/v1/user/<uuid>"
+        """
+        user = storage.get_all(User)[0]
+        res = self.__class__.client.get(f"/api/v1/users/{user.id}")
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.json, user.to_dict())
