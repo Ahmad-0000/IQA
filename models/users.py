bcad773be934afdff06dffc44eccb8a99dd73dba
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-04 14:20:24 +0000

    Ensure unique emails

--- models/users.py
+++ models/users.py
@@ -21,7 +21,7 @@ class User(BaseModel, Base):
     middle_name = Column(String(20), nullable=False)
     last_name = Column(String(20), nullable=False)
     dob = Column(Date, nullable=False)
-    email = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
     image_path = Column(String(256), nullable=True, default=None)
     bio = Column(String(300), nullable=True)
