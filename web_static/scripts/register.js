77458b8dd4f1873f491c53a8a23c8b5ef6a2c7b1
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-07 20:11:58 +0000

    Add registeration page

---
+++ web_static/scripts/register.js
@@ -0,0 +1,32 @@
+// Handle registeration
+
+const fName = document.querySelector("#f-name")
+const mName = document.querySelector("#m-name")
+const lName = document.querySelector("#l-name")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const dob = document.querySelector("#dob")
+const registerBtn = document.querySelector("#submit")
+
+registerBtn.addEventListener('click', () => {
+  const data = {
+    "first_name": fName.value,
+    "middle_name": mName.value,
+    "last_name": lName.value,
+    "email": email.value,
+    "password": password.value,
+    "dob": dob.value,
+    "agree": agree.checked
+  }
+  fetch("http://localhost:5001/api/v1/users", {
+    cretentials: "include",
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(data)
+  }, (res) => {
+    console.log(res);
+    if (res.ok) {
+      window.location = "/profile.html";
+    }
+  });
+});
