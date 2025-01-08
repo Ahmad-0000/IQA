f70818adc87548e5babab6ecf469789382bd2eb1
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-08 04:47:41 +0000

    Remove a bug

--- web_dynamic/scripts/register.js
+++ web_dynamic/scripts/register.js
@@ -22,7 +22,7 @@ myForm.addEventListener('submit', (event) => {
     data['agree'] = true;
     fetch("http://localhost:5001/api/v1/users", {
       method: 'POST',
-      headers: 'Content-Type: application/json',
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(data),
       credentials: 'include',
     }).then((res) => {
