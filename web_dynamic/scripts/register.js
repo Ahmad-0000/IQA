70f3209b50588296f917196363ae943961e25933
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-09 20:12:06 +0000

    Easir syntax

--- web_dynamic/scripts/register.js
+++ web_dynamic/scripts/register.js
@@ -1,8 +1,6 @@
 // Handle registeration
 
 const errorPage = document.querySelector("#error");
-const errorCode = document.querySelector("#error h1 span");
-const errorMessage = document.querySelector("#error p:first-of-type");
 const fName = document.querySelector("#f-name")
 const mName = document.querySelector("#m-name")
 const lName = document.querySelector("#l-name")
@@ -28,9 +26,8 @@ myForm.addEventListener('submit', (event) => {
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify(data),
       credentials: 'include',
-    })
-       .then((res) => {
-          if (res.status === 201) {
+    }).then((res) => {
+          if (res.ok) {
             document.location = "/profile.html?user_id=me";
           } else {
             showErrorPage(res);
@@ -41,6 +38,8 @@ myForm.addEventListener('submit', (event) => {
 
 async function showErrorPage(response) {
   // Show the error page
+  const errorCode = document.querySelector("#error h1 span");
+  const errorMessage = document.querySelector("#error p:first-of-type");
   errorPage.style.display = "flex";
   const data = await response.json();
   errorCode.textContent = data.status;
