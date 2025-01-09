7047744cd42965c1fc4af0e41f168dab8635ee9b
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-09 03:11:09 +0000

    Filter quizzes

--- web_dynamic/scripts/index.js
+++ web_dynamic/scripts/index.js
@@ -5,6 +5,8 @@ const modifiersCloser = document.getElementById("modifiers-closer");
 const categories = document.getElementById("search-categories");
 const categoriesPopover = document.getElementById("categories-popover");
 const categoriesCloser = document.getElementById("categories-closer");
+const quizzesWrapper = document.querySelector(".quizzes-wrapper")
+
 
 modifiers.addEventListener('click', () => {
     if (categoriesPopover.style.display === "block") {
@@ -66,3 +68,115 @@ cats.forEach((hoveredCat) => {
         });
     });
 });
+
+
+// Fetch categories and give ids
+
+fetch("http://localhost:5001/api/v1/categoreis").then(res => {
+    if (res.status == 200) {
+        return res.json()
+    }
+}).then((categoreis) => {
+    for (const cat of categoreis) {
+        const htmlCat = document.querySelector(`#categories i[title='${cat.name}']`);
+        htmlCat.setAttribute('data-id', cat.id);
+    }
+})
+
+
+const dateFilter = document.querySelector(".filters div:first-of-type");
+const orderTypeDiv = document.querySelector(".filters div:nth-child(2)");
+const orderTypeElement = document.querySelector(".filters div:nth-child(2) i");
+const popularityFilter = document.querySelector(".filters div:last-of-type");
+const filterTrigger = document.querySelector(".filters > i:last-child")
+let filterCats = [];
+let orderType = "desc";
+let orderAttribute = "";
+
+dateFilter.addEventListener('click', () => {
+    if (dateFilter.hasAttribute("checked")) {
+        dateFilter.removeAttribute("checked");
+        orderAttribute = ""
+    } else {
+        if (popularityFilter.hasAttribute("checked")) {
+            popularityFilter.removeAttribute("checked")
+        }
+        dateFilter.setAttribute("checked", true);
+        orderAttribute = "added_at";
+    }
+});
+
+popularityFilter.addEventListener('click', () => {
+    if (popularityFilter.hasAttribute("checked")) {
+        popularityFilter.removeAttribute("checked");
+        orderAttribute = ""
+    } else {
+        if (dateFilter.hasAttribute("checked")) {
+            dateFilter.removeAttribute("checked");
+        }
+        popularityFilter.setAttribute("checked", true);
+        orderAttribute = "times_taken";
+    }
+});
+
+orderTypeDiv.addEventListener('click', () => {
+    if (orderTypeElement.getAttribute("order-type") === "desc") {
+        orderTypeElement.setAttribute("order-type", "asc");
+        orderType = "asc";
+    } else if (orderTypeElement.getAttribute("order-type") === 'asc') {
+        orderTypeElement.setAttribute("order-type", "desc");
+        orderType = "desc";
+    }
+});
+
+for (const cat of cats) {
+    cat.addEventListener('click', () => {
+        if (cat.hasAttribute("checked")) {
+            cat.removeAttribute("checked");
+            filterCats = filterCats.filter((element) => cat.getAttribute("title") !== element);
+
+        } else {
+            cat.setAttribute("checked", "true");
+            filterCats.push(cat.getAttribute("title"));
+        }
+    });
+}
+
+filterTrigger.addEventListener('click', () => {
+    let url;
+    if (! orderAttribute) {
+        orderAttribute = "added_at";
+    }
+    if (! orderType) {
+        orderType = "desc";
+    }
+    if (filterCats.length === 0) {
+        url = `http://localhost:5001/api/v1/quizzes?order_attribute=${orderAttribute}?order_type=${orderType}`;
+    } else {
+        url = `http://localhost:5001/api/v1/quizzes?cats=${filterCats.join(',')}?order_attribute=${orderAttribute}?order_type=${orderType}`;
+    }
+    fetch(url).then((res) => {
+        if (res.ok) {
+            return res.json();
+        } else {
+            // Show error page
+            console.log(res);
+        }
+    }).then((quizzes) => {
+        for (const quiz of quizzes) {
+            console.log(quiz);
+        }
+    })
+});
+
+// <div class="quiz" data-id="">
+//                         <div class="img"></div>
+//                         <div class="info">
+//                             <h2>Title</h2>
+//                             <div class="stats">
+//                                 <i class="fa-regular fa-heart"><span> 10 Likes</span></i>
+//                                 <i class="fa-solid fa-repeat"><span> 10 times</span></i>
+//                                 <i class="fa-regular fa-clock"><span> 10 Minutes</span></i>
+//                             </div>
+//                         </div>
+//                     </div>
