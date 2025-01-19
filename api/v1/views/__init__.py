fce798c6567edd1e9785be353f03f52840e70faf
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-19 09:08:46 +0000

    Remove dangerous and unnecessary points
    See the few commit messages before these for more info

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,9 +6,6 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__)
 from api.v1.views.user import *
 from api.v1.views.quiz import *
-from api.v1.views.question import *
-from api.v1.views.answer import *
 from api.v1.views.feedback import *
-from api.v1.views.category import *
 from api.v1.views.like import *
 from api.v1.views.session_auth import *
