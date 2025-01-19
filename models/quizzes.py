62b96e2af33c270feb8376e3e754f2382192629a
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-19 09:28:22 +0000

    Handle date relationships properly
    Scores are associated with users and quizzes and no relationship
    with a certain quiz is made, because this makes no sense.
    
    Categories are stored as a string field in the quiz record.
    
    Capitalize difficulty tag names.
    
    Allocate just the necessary storage to store image pathes

--- models/quizzes.py
+++ models/quizzes.py
@@ -16,9 +16,9 @@ class Quiz(BaseModel, Base):
     times_taken = Column(Integer, nullable=False, default=0)
     likes_num = Column(Integer, nullable=False, default=0)
     duration = Column(Integer, nullable=False, default=5)
-    image_path = Column(String(256), nullable=True, default=None)
-    difficulty = Column(Enum('easy', 'medium', 'hard'), nullable=False)
-    category_id = Column(String(36), ForeignKey('categories.id'), nullable=True)
+    image_path = Column(String(57), nullable=True, default=None)
+    difficulty = Column(Enum('Easy', 'Medium', 'Hard'), nullable=False)
+    category = Column(String(20), nullable=True)
     user_id = Column(String(36), ForeignKey('users.id'), nullable=False)
     user = relationship('User', back_populates='quizzes')
     questions = relationship(
@@ -31,10 +31,7 @@ class Quiz(BaseModel, Base):
                 cascade='all, delete, delete-orphan',
                 back_populates='quiz'
             )
-    scores = relationship(
-                'Score',
-                cascade='all, delete, delete-orphan',
-                back_populates='quiz'
-            )
-    fans_users: Mapped[List['User']] = relationship(secondary=quizzes_likes, back_populates='liked_quizzes')
-    category = relationship('Category', back_populates='quizzes')
+    fans_users: Mapped[List['User']] = relationship(
+                                            secondary=quizzes_likes,
+                                            back_populates='liked_quizzes'
+                                        )
