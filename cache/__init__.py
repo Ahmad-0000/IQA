cc46e1762b9ed7b8cf7bda0c442c2745fb07c81d
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-21 07:05:07 +0000

    Populate the cache when module initialization

--- cache/__init__.py
+++ cache/__init__.py
@@ -2,5 +2,8 @@
 """
 from cache.redis_stack_cache import RedisStackCache
 
-
+RedisStackCache._pool_size = 5
 cache_client = RedisStackCache()
+cache_client.populate_quizzes_pool('newest')
+cache_client.populate_quizzes_pool('oldest')
+cache_client.populate_popular_pool()
