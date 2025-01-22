fb0f31d93e8f8fa5aa7db658924330d983930221
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-22 12:10:00 +0000

    Simplify the code

--- cache/redis_stack_cache.py
+++ cache/redis_stack_cache.py
@@ -403,35 +403,24 @@ class RedisStackCache():
         if not session:
             return 404
         quiz_id = session['quiz_id']
+        if int(datetime.utcnow().timestamp()) >= session['deadline'] or idx == -1:
+            score = session['score']
+            score = Score(score=score, user_id=user_id, quiz_id=quiz_id)
+            score.save()
+            snapshots = session['snapshots']
+            snapshot_ids = register_snapshots(
+                                                score_id=score.id,
+                                                user_id=session['user_id'],
+                                                quiz_id=['quiz_id'],
+                                                snapshots=snapshots.items()
+            )
+            RedisStackCache.__client.json().delete('session_cookie')
+            RedisStackCache.__client.srem(f'ongoing_quiz_tracker:{quiz_id}', session_cookie)
         if int(datetime.utcnow().timestamp()) >= session['deadline']:
-            score = session['score']
-            score = Score(score=score, user_id=user_id, quiz_id=quiz_id)
-            score.save()
-            snapshots = session['snapshots']
-            snapshot_ids = register_snapshots(
-                                                score_id=score.id,
-                                                user_id=session['user_id'],
-                                                quiz_id=['quiz_id'],
-                                                snapshots=snapshots.items()
-            )
-            RedisStackCache.__client.json().delete('session_cookie')
-            RedisStackCache.__client.srem(f'ongoing_quiz_tracker:{quiz_id}', session_cookie)
             return (404, snapshot_ids)
-        ongoing_quiz = RedisStackCache.__client.json().get(f'ongoing:{quiz_id}')
-        if idx == -1:
-            score = session['score']
-            score = Score(score=score, user_id=user_id, quiz_id=quiz_id)
-            score.save()
-            snapshots = session['snapshots']
-            snapshot_ids = register_snapshots(
-                                                score_id=score.id,
-                                                user_id=session['user_id'],
-                                                quiz_id=['quiz_id'],
-                                                snapshots=snapshots.items()
-            )
-            RedisStackCache.__client.json().delete('session_cookie')
-            RedisStackCache.__client.srem(f'ongoing_quiz_tracker:{quiz_id}', session_cookie)
+        elif idx == -1:
             return (201, score.score)
+        ongoing_quiz = RedisStackCache.__client.json().get(f'ongoing:{quiz_id}')
         try:
             return ongoing_quiz['questions'][idx]
         except IndexError:
