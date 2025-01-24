f442301fd92a0c306402d798f4a24c5072ccf32f
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-24 06:02:17 +0000

    Test models CRUD operations

--- tests/test_models/test_crud_operations.py
+++ tests/test_models/test_crud_operations.py
@@ -1,9 +1,12 @@
-"""Test CRUD operations using a "User" class instance
-NOTE: "Reading" operation will not be tested here, it will
-be tested as an engine funcionality
+"""Test CRUD operations on classes inhereting from "Base"
+    using a "User" class instance.
+
+    NOTE: "Reading" operation will not be tested here, it will
+    be tested as an engine funcionality
 """
 import bcrypt
 import unittest
+from unittest.mock import patch
 from datetime import date
 from models.users import User
 import MySQLdb
@@ -24,7 +27,28 @@ class TestUser(unittest.TestCase):
     def setUpClass(cls):
         """Execute for class initalization
         """
-        cls.user = User(first_name="Ahmad",
+        cls.db_connection = MySQLdb.connect(
+                    user=user,
+                    passwd=pawd,
+                    host=host,
+                    port=port,
+                    db=db
+        )
+        cls.cursor = cls.db_connection.cursor()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Execute for class clean up
+        """
+        cls.cursor.close()
+        cls.db_connection.close()
+
+    def test_attributes(self):
+        """Test normal initialization
+        """
+        with patch('models.Storage'):
+            user = User(
+                         first_name="Ahmad",
                          middle_name="Husain",
                          last_name="Basheer",
                          dob=date(2005, 3, 5),
@@ -32,7 +56,66 @@ class TestUser(unittest.TestCase):
                          password="fakepassword",
                          image_path="somepath",
                          bio="A person seeking to be a software engineer"
+            )
+            user.save()
+            expected_path = f"/data/iqa/images/user/{user.id}"
+            expected_bio = "A person seeking to be a software engineer"
+            self.assertEqual(user.first_name, "Ahmad")
+            self.assertEqual(user.middle_name, "Husain")
+            self.assertEqual(user.last_name, "Basheer")
+            self.assertEqual(user.dob, date(2005, 3, 5))
+            self.assertEqual(user.email, "ahmad.new.m.v@gmail.com")
+            self.assertTrue(bcrypt.checkpw(bytes('fakepassword', 'utf-8'),
+                            bytes(user.password, 'utf-8')))
+            self.assertEqual(user.image_path, expected_path)
+            self.assertEqual(user.bio, expected_bio)
+            self.assertEqual(user.liked_quizzes_num, 0)
+            self.assertEqual(user.quizzes_made, 0)
+            self.assertEqual(user.quizzes_taken, 0)
+
+    def test_db_creation(self):
+        """Test object creation in the db
+        """
+        self.cursor.execute('SELECT COUNT(*) FROM users;')
+        initial_rows_number = self.cursor.fetchone()[0]
+        self.db_connection.commit()
+        user = User(
+                         first_name="Ahmad",
+                         middle_name="Husain",
+                         last_name="Basheer",
+                         dob=date(2005, 3, 5),
+                         email="ahmadhusain5002@gmail.com",
+                         password="fakepassword",
+                         image_path="somepath",
+                         bio="A person seeking to be a software engineer"
         )
+        user.save()
+        self.cursor.execute('SELECT COUNT(*) FROM users;')
+        new_rows_number = self.cursor.fetchone()[0]
+        self.db_connection.commit()
+        self.assertEqual(initial_rows_number + 1, new_rows_number)
+
+    def test_db_update(self):
+        """Test object data update in the db
+        """
+        user = User(
+                         first_name="Ahmad",
+                         middle_name="Husain",
+                         last_name="Basheer",
+                         dob=date(2005, 3, 5),
+                         email="ahmadfruit311@gmail.com",
+                         password="fakepassword",
+                         image_path="somepath",
+                         bio="A person seeking to be a software engineer"
+        )
+        user.save()
+        self.db_connection.commit()
+        self.cursor.execute(f'SELECT first_name FROM users WHERE id = "{user.id}"')
+        initial_first_name = self.cursor.fetchone()[0]
+        user.update(first_name='Mohammad')
+        self.db_connection.commit()
+        self.cursor.execute(f'SELECT first_name FROM users WHERE id = "{user.id}"')
+        new_first_name = self.cursor.fetchone()[0]
         cls.user.save()
         cls.db_connection = MySQLdb.connect(
                     user=user,
@@ -47,27 +130,8 @@ class TestUser(unittest.TestCase):
     def tearDownClass(cls):
         """Execute for class clean up
         """
-        cls.user.delete()
         cls.cursor.close()
         cls.db_connection.close()
-
-    def test_attributes(self):
-        """Test normal initialization
-        """
-        expected_path = f"/data/iqa/images/user/{self.user.id}"
-        expected_bio = "A person seeking to be a software engineer"
-        self.assertEqual(self.user.first_name, "Ahmad")
-        self.assertEqual(self.user.middle_name, "Husain")
-        self.assertEqual(self.user.last_name, "Basheer")
-        self.assertEqual(self.user.dob, date(2005, 3, 5))
-        self.assertEqual(self.user.email, "ahmad.new.m.v@gmail.com")
-        self.assertTrue(bcrypt.checkpw(bytes('fakepassword', 'utf-8'),
-                        bytes(self.user.password, 'utf-8')))
-        self.assertEqual(self.user.image_path, expected_path)
-        self.assertEqual(self.user.bio, expected_bio)
-        self.assertEqual(self.user.liked_quizzes_num, 0)
-        self.assertEqual(self.user.quizzes_made, 0)
-        self.assertEqual(self.user.quizzes_taken, 0)
 
     def test_db_creation(self):
         """Test object creation in the db
